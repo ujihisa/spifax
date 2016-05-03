70d5d162ab1b220bba1ec842c6be3efb86e5ed6e
Tatsuhiro Ujihisa <ujihisa@gmail.com>
2016-05-02 23:28:49 -0700

    Update dependencies

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.0"]
+                 [ring/ring-defaults "0.2.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler spifax.handler/app}
   :profiles
