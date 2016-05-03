e589b13134d0b187556679de3e9702e9a5d32a4f
Tatsuhiro Ujihisa <ujihisa@gmail.com>
2016-05-03 00:01:10 -0700

    Add spigot repo

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
+  :repositories {"spigot-repo"
+                 "https://hub.spigotmc.org/nexus/content/repositories/snapshots/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.0"]
                  [ring/ring-defaults "0.2.0"]]
