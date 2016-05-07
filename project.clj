62bd0376f5707bf7943b16ddeb631a6958ed2441
Tatsuhiro Ujihisa <ujihisa@gmail.com>
2016-05-06 23:50:21 -0700

    Include spigot as a dependency

--- project.clj
+++ project.clj
@@ -3,12 +3,19 @@
   :url "http://example.com/FIXME"
   :license {:name "GPL3 or any later versions"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :min-lein-version "2.0.0"
-  :repositories {"spigot-repo"
-                 "https://hub.spigotmc.org/nexus/content/repositories/snapshots/"}
+  :min-lein-version "2.5.0"
+  :repositories {"org.bukkit"
+                 "http://repo.bukkit.org/content/groups/public/"
+                 "spigot-repo"
+                 "https://hub.spigotmc.org/nexus/content/repositories/snapshots/"
+                 "localrepo1"
+                 "file://myrepo"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.0"]
-                 [ring/ring-defaults "0.2.0"]]
+                 [ring/ring-defaults "0.2.0"]
+                 [org.spigotmc/spigot-api "1.9.2-R0.1-SNAPSHOT"]
+                 [org.spigotmc/spigot "1.9.2"]
+                 [clj-http "3.0.1"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler spifax.handler/app}
   :profiles
