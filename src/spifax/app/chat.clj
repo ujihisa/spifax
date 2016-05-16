c369a37fc664f26d52ff6dbf2c0d023d5327456c
Tatsuhiro Ujihisa <ujihisa@gmail.com>
2016-05-15 20:15:19 -0700

    Don't show same message again

--- src/spifax/app/chat.clj
+++ src/spifax/app/chat.clj
@@ -1,10 +1,6 @@
 (ns spifax.app.chat
   (:require [spifax.lib :as l]))
 
-(def ^:private bot-verifier (System/getenv "BOT_VERIFIER"))
-
-(prn :bot-verifier bot-verifier)
-
 (defn org.bukkit.event.player.AsyncPlayerChatEvent [event]
   (let [player (.getPlayer event)]
     (prn 'chat player event)))
