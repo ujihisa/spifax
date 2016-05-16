394ccf60fe4a06ecda43c2e034cdf5c3fa223972
Tatsuhiro Ujihisa <ujihisa@gmail.com>
2016-05-15 19:45:15 -0700

    Show bot-verifier

--- src/spifax/app/chat.clj
+++ src/spifax/app/chat.clj
@@ -3,6 +3,8 @@
 
 (def ^:private bot-verifier (System/getenv "BOT_VERIFIER"))
 
+(prn :bot-verifier bot-verifier)
+
 (defn org.bukkit.event.player.AsyncPlayerChatEvent [event]
   (let [player (.getPlayer event)]
     (prn 'chat player event)))
