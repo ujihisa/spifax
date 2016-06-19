e732e7ef9eeaf08588582e88d3f5195a5c97b379
ujihisa <ujihisa@gmail.com>
2016-06-18 20:16:01 -0700

    Debugging

--- src/spifax/app/chat.clj
+++ src/spifax/app/chat.clj
@@ -8,8 +8,8 @@
 (defn org.bukkit.event.player.AsyncPlayerChatEvent' [event get-player get-name get-format]
   (let [player (get-player event)]
     (prn 'chat player event)
-    (l/post-lingr (format (get-format event) (get-name player) message))))
+    (l/post-lingr-sync (format (get-format event) (get-name player) message))))
 
 (defn org.bukkit.event.player.PlayerLoginEvent [event]
   (prn 'org.bukkit.event.player.PlayerLoginEvent)
-  (l/post-lingr (format "[LOGIN] %s logged in." (.getName (.getPlayer event)))))
+  (l/post-lingr-sync (format "[LOGIN] %s logged in." (.getName (.getPlayer event)))))
