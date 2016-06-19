4f9976d77600b899061766de605a765f3c14268e
ujihisa <ujihisa@gmail.com>
2016-06-18 21:06:10 -0700

    Get 64 diamonds when you open inventory for the first time

--- src/spifax/app/bonus_achievement.clj
+++ src/spifax/app/bonus_achievement.clj
@@ -1,12 +1,20 @@
 (ns spifax.app.bonus-achievement
-  (:require [sugot.lib :as l]))
+  (:require [sugot.lib :as l]
+            [sugot.world :as w])
+  (:import [org.bukkit Achievement Material]
+           [org.bukkit.inventory ItemStack]))
 
-(defn org.bukkit.event.player.PlayerAchievementAwardedEvent [event get-player get-achievement]
+(defn org.bukkit.event.player.PlayerAchievementAwardedEvent' [event get-player get-achievement get-location]
   (l/post-lingr (format "[ACHIEVEMENT] %s got %s"
                         (get-player event)
-                        (get-achievement event))))
+                        (get-achievement event)))
+  (case (get-achievement event)
+    Achievement/OPEN_INVENTORY (let [loc (get-location player)]
+                                 (w/strike-lightning-effect loc)
+                                 (w/drop-item loc (ItemStack. Material/DIAMOND 64)))
+    nil))
 
 (defn org.bukkit.event.player.PlayerAchievementAwardedEvent [event]
   (org.bukkit.event.player.PlayerAchievementAwardedEvent'
-    event #(.getPlayer %) #(.getAchievement %)))
+    event #(.getPlayer %) #(.getAchievement %) #(.getLocation %)))
 
