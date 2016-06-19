227ca868f5497dd60bf0956a7d6b7fc84cd6b5ec
ujihisa <ujihisa@gmail.com>
2016-06-18 20:18:49 -0700

    Reorder to be able to find

--- src/spifax/app/chat.clj
+++ src/spifax/app/chat.clj
@@ -1,14 +1,14 @@
 (ns spifax.app.chat
   (:require [sugot.lib :as l]))
 
-(defn org.bukkit.event.player.AsyncPlayerChatEvent [event]
-  (org.bukkit.event.player.AsyncPlayerChatEvent*
-    event #(.getPlayer %) #(.getName %) #(.getFormat %)))
-
-(defn org.bukkit.event.player.AsyncPlayerChatEvent* [event get-player get-name get-format]
+(defn org.bukkit.event.player.AsyncPlayerChatEvent' [event get-player get-name get-format]
   (let [player (get-player event)]
     (prn 'chat player event)
     (l/post-lingr-sync (format (get-format event) (get-name player) message))))
+
+(defn org.bukkit.event.player.AsyncPlayerChatEvent [event]
+  (org.bukkit.event.player.AsyncPlayerChatEvent'
+    event #(.getPlayer %) #(.getName %) #(.getFormat %)))
 
 (defn org.bukkit.event.player.PlayerLoginEvent [event]
   (prn 'org.bukkit.event.player.PlayerLoginEvent)
