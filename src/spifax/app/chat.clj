fde331996235685bbfbeeb452b55e462d3072ff6
ujihisa <ujihisa@gmail.com>
2016-06-18 20:12:50 -0700

    Post chat to lingr

--- src/spifax/app/chat.clj
+++ src/spifax/app/chat.clj
@@ -2,8 +2,13 @@
   (:require [sugot.lib :as l]))
 
 (defn org.bukkit.event.player.AsyncPlayerChatEvent [event]
-  (let [player (.getPlayer event)]
-    (prn 'chat player event)))
+  (org.bukkit.event.player.AsyncPlayerChatEvent'
+    event #(.getPlayer %) #(.getName %) #(.getFormat %)))
+
+(defn org.bukkit.event.player.AsyncPlayerChatEvent' [event get-player get-name get-format]
+  (let [player (get-player event)]
+    (prn 'chat player event)
+    (l/post-lingr (format (get-format event) (get-name player) message))))
 
 (defn org.bukkit.event.player.PlayerLoginEvent [event]
   (prn 'org.bukkit.event.player.PlayerLoginEvent)
