d826f86a3785ab900d0f58d0af107e0f3638d9ae
ujihisa <ujihisa@gmail.com>
2016-06-18 20:32:02 -0700

    Avoid confusion with class

--- src/spifax/app/chat.clj
+++ src/spifax/app/chat.clj
@@ -1,14 +1,14 @@
 (ns spifax.app.chat
   (:require [sugot.lib :as l]))
 
-(defn org.bukkit.event.player.AsyncPlayerChatEvent' [event get-player get-name get-format]
+(defn org.bukkit.event.player.AsyncPlayerChatEvent' [event get-player get-name get-format get-message]
   (let [player (get-player event)]
     (prn 'chat player event)
-    (l/post-lingr-sync (format (get-format event) (get-name player) message))))
+    (l/post-lingr-sync (format (get-format event) (get-name player) (get-message event)))))
 
 (defn org.bukkit.event.player.AsyncPlayerChatEvent [event]
-  (org.bukkit.event.player.AsyncPlayerChatEvent'
-    event #(.getPlayer %) #(.getName %) #(.getFormat %)))
+  (#'org.bukkit.event.player.AsyncPlayerChatEvent'
+    event #(.getPlayer %) #(.getName %) #(.getFormat %) #(.getMessage %)))
 
 (defn org.bukkit.event.player.PlayerLoginEvent [event]
   (prn 'org.bukkit.event.player.PlayerLoginEvent)
