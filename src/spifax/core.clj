a29943577d45fd3768624833639a3705cd32851d
Tatsuhiro Ujihisa <ujihisa@gmail.com>
2016-06-24 17:11:14 -0700

    Dynamically load events

--- src/spifax/core.clj
+++ src/spifax/core.clj
@@ -7,30 +7,16 @@
 
 (defn- register-all-events [plugin-manager]
   (prn 'register-all-events plugin-manager)
-  (let [namespace* 'spifax.app.chat
-        klass org.bukkit.event.player.AsyncPlayerChatEvent
-        sym (symbol (format "%s/%s"
-                            (name namespace*)
-                            (.getName klass)))
-        _ (require namespace*)
-        f (ns-resolve namespace* sym)]
-    (sugot.core/register-event plugin-manager klass f))
-  (let [namespace* 'spifax.app.chat
-        klass org.bukkit.event.player.PlayerLoginEvent
-        sym (symbol (format "%s/%s"
-                            "spifax.app.chat"
-                            (.getName klass)))
-        _ (require namespace*)
-        f (ns-resolve namespace* sym)]
-    (sugot.core/register-event plugin-manager klass f))
-  (let [namespace* 'spifax.app.bonus-achievement
-        klass org.bukkit.event.player.PlayerAchievementAwardedEvent
-        sym (symbol (format "%s/%s"
-                            "spifax.app.bonus-achievement"
-                            (.getName klass)))
-        _ (require namespace*)
-        f (ns-resolve namespace* sym)]
-    (sugot.core/register-event plugin-manager klass f)))
+  (doseq [namespace* ['spifax.app.chat 'spifax.app.bonus-achievement]
+          klass [org.bukkit.event.player.AsyncPlayerChatEvent
+                 org.bukkit.event.player.PlayerLoginEvent
+                 org.bukkit.event.player.PlayerAchievementAwardedEvent]]
+    (let [sym (symbol (format "%s/%s"
+                              (name namespace*)
+                              (.getName klass)))]
+      (require namespace*)
+      (when-let [f (ns-resolve namespace* sym)]
+        (sugot.core/register-event plugin-manager klass f)))))
 
 (defn- start
   "It's called right after minecraft server is ready"
