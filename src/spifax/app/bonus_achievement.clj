fdc20181bd2a9d61d4fb7d4f989cdb414895fe88
Tatsuhiro Ujihisa <ujihisa@gmail.com>
2016-06-24 17:31:47 -0700

    Add more achievements

--- src/spifax/app/bonus_achievement.clj
+++ src/spifax/app/bonus_achievement.clj
@@ -11,15 +11,29 @@
   (condp = (get-achievement event)
     Achievement/OPEN_INVENTORY (let [loc (get-location (get-player event))]
                                  (w/strike-lightning-effect loc)
+                                 (l/post-lingr "You got 64 diamonds.")
                                  (w/drop-item loc (ItemStack. Material/DIAMOND 64)))
     Achievement/MAKE_BREAD (let [loc (get-location (get-player event))]
                              (w/strike-lightning-effect loc)
                              (dotimes [_ 2]
+                               (l/post-lingr "You got 128 breads")
                                (w/drop-item loc (ItemStack. Material/BREAD 64))))
     Achievement/ENCHANTMENTS (let [loc (get-location (get-player event))]
                                (w/strike-lightning-effect loc)
-                               (.sendMessage (get-player event) "[ACHIEVEMENT] Level Up Bonus!")
+                               (l/post-lingr "You got 80 more levels")
                                (add-level (get-player event) 80))
+    Achievement/NETHER_PORTAL (let [loc (get-location (get-player event))]
+                                (w/strike-lightning-effect loc)
+                                (l/post-lingr "You got 64 glowstones")
+                                (w/drop-item loc (ItemStack. Material/GLOWSTONE 64)))
+    Achievement/GET_BLAZE_ROD (let [loc (get-location (get-player event))]
+                                (w/strike-lightning-effect loc)
+                                (l/post-lingr "You got 64 blaze rods")
+                                (w/drop-item loc (ItemStack. Material/BLAZE_ROD 64)))
+    Achievement/COOK_FISH (let [loc (get-location (get-player event))]
+                            (w/strike-lightning-effect loc)
+                            (l/post-lingr "You got 64 pufferfishes")
+                            (w/drop-item loc (ItemStack. Material/RAW_FISH 64 (short 0) (byte 3))))
     nil))
 
 (defn org.bukkit.event.player.PlayerAchievementAwardedEvent [event]
