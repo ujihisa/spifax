896260aa99ead1733e9681c8b692fe9a9836b25f
ujihisa <ujihisa@gmail.com>
2016-06-24 19:30:19 -0700

    Drop a gold from enemy

--- src/spifax/app/gold.clj
+++ src/spifax/app/gold.clj
@@ -1,18 +1,22 @@
 (ns spifax.app.gold
   (:import [org.bukkit.entity Monster]
-           [org.bukkit Material])
+           [org.bukkit Material]
+           [org.bukkit.inventory ItemStack])
   (:require [sugot.lib :as l]
             [sugot.world :as w]))
 
-(defn org.bukkit.event.entity.EntityDeathEvent' [entity drops]
+(defn org.bukkit.event.entity.EntityDeathEvent' [entity drops get-location]
   (condp instance? entity
     Monster
     (let [equipment (.getEquipment entity)]
       (doseq [armour (into [] (.getArmorContents equipment))
               :when (.startsWith (.toString (.getType armour)) "GOLD")]
-        (prn :cool armour)))
+        (l/post-lingr (format "[GOLD] %s dropped a gold."
+                              (class entity)))
+        (w/drop-item (get-location entity) (ItemStack. Material/GOLD_INGOT 1))))
     nil))
 
 (defn org.bukkit.event.entity.EntityDeathEvent [event]
   (#'org.bukkit.event.entity.EntityDeathEvent'
-    (.getEntity event) (.getDrops event)))
+    (.getEntity event) (.getDrops event)
+    #(fn [entity] (.getLocation entity))))
