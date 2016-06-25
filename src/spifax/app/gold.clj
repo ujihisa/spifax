89190befb79ade443ab8c4020c2f978f82372df3
ujihisa <ujihisa@gmail.com>
2016-06-24 20:13:37 -0700

    Don't use wrong `replace`

--- src/spifax/app/gold.clj
+++ src/spifax/app/gold.clj
@@ -3,7 +3,8 @@
            [org.bukkit Material]
            [org.bukkit.inventory ItemStack])
   (:require [sugot.lib :as l]
-            [sugot.world :as w]))
+            [sugot.world :as w]
+            [clojure.string]))
 
 (defn org.bukkit.event.entity.EntityDeathEvent' [entity drops get-location get-name get-killer]
   (condp instance? entity
@@ -12,10 +13,10 @@
       (doseq [armour (into [] (.getArmorContents equipment))
               :when (.startsWith (.toString (.getType armour)) "GOLD")]
         (l/post-lingr (format "[GOLD] %s killed by %s dropped a gold."
-                              (replace (.getSimpleName (class entity)) #"^Craft" "")
+                              (clojure.string/replace (.getSimpleName (class entity)) #"^Craft" "")
                               (get-name (get-killer entity))))
         (l/broadcast (format "[GOLD] %s killed by %s dropped a gold."
-                             (replace (.getSimpleName (class entity)) #"^Craft" "")
+                             (clojure.string/replace (.getSimpleName (class entity)) #"^Craft" "")
                            (get-name (get-killer entity))))
         (w/drop-item (get-location entity) (ItemStack. Material/GOLD_INGOT 1))))
     nil))
