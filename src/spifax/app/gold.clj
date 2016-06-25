7e04c9d2bf37c9e1e774d5119e652da688291f5d
ujihisa <ujihisa@gmail.com>
2016-06-24 19:38:51 -0700

    Mention the killer's name

--- src/spifax/app/gold.clj
+++ src/spifax/app/gold.clj
@@ -5,18 +5,24 @@
   (:require [sugot.lib :as l]
             [sugot.world :as w]))
 
-(defn org.bukkit.event.entity.EntityDeathEvent' [entity drops get-location]
+(defn org.bukkit.event.entity.EntityDeathEvent' [entity drops get-location get-name get-killer]
   (condp instance? entity
     Monster
     (let [equipment (.getEquipment entity)]
       (doseq [armour (into [] (.getArmorContents equipment))
               :when (.startsWith (.toString (.getType armour)) "GOLD")]
-        (l/post-lingr (format "[GOLD] %s dropped a gold."
-                              (.getSimpleName (class entity))))
+        (l/post-lingr (format "[GOLD] %s, killed by %s, dropped a gold."
+                              (.getSimpleName (class entity))
+                              (get-name (get-killer entity))))
+        (l/broadcast (format "[GOLD] %s, killed by %s, dropped a gold."
+                           (.getSimpleName (class entity))
+                           (get-name (get-killer entity))))
         (w/drop-item (get-location entity) (ItemStack. Material/GOLD_INGOT 1))))
     nil))
 
 (defn org.bukkit.event.entity.EntityDeathEvent [event]
   (#'org.bukkit.event.entity.EntityDeathEvent'
     (.getEntity event) (.getDrops event)
-    #(fn [entity] (.getLocation entity))))
+    #(fn [entity] (.getLocation entity))
+    #(fn [player] (.getName player))
+    #(fn [living-entity] (.getKiller living-entity))))
