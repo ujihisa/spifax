45a46fe13c1aff682af49f48e13bd0662f821217
ujihisa <ujihisa@gmail.com>
2016-06-26 14:06:37 -0700

    Send message directly to the killer

--- src/spifax/app/gold.clj
+++ src/spifax/app/gold.clj
@@ -12,12 +12,10 @@
     (let [equipment (.getEquipment entity)]
       (doseq [armour (into [] (.getArmorContents equipment))
               :when (.startsWith (.toString (.getType armour)) "GOLD")]
-        #_(l/post-lingr (format "[GOLD] %s killed by %s dropped a gold."
+        (.sendMessage (get-killer entity)
+                      (format "[GOLD] %s killed by %s dropped a gold."
                               (clojure.string/replace (.getSimpleName (class entity)) #"^Craft" "")
                               (get-name (get-killer entity))))
-        (l/broadcast (format "[GOLD] %s killed by %s dropped a gold."
-                             (clojure.string/replace (.getSimpleName (class entity)) #"^Craft" "")
-                           (get-name (get-killer entity))))
         (w/drop-item (get-location entity) (ItemStack. Material/GOLD_INGOT 1))))
     nil))
 
