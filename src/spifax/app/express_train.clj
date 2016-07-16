1318e4ec1c2c05dff535abf82ef76fd8f1208de1
ujihisa <ujihisa@gmail.com>
2016-07-16 16:52:09 -0700

    Stablize express_train
    * Lots of bugfixes

--- src/spifax/app/express_train.clj
+++ src/spifax/app/express_train.clj
@@ -2,12 +2,15 @@
   (:require [sugot.lib :as l]
             [sugot.world :as w])
   (:import [org.bukkit Bukkit Material Effect Sound]
-           [org.bukkit.entity Player Minecart]))
+           [org.bukkit.entity Player Minecart]
+           [org.bukkit.util Vector]))
 
 (defn rand-around [loc]
   (.add loc (- 0.5 (rand)) (- 0.5 (rand)) (- 0.5 (rand))))
 
 (def minecart-max-speed 2.05) ; precisely 2.054000
+
+(def minecart-default-flying-velocity-mod (Vector. 0.95 0.95 0.95))
 
 (defn-
   ^{:doc "(find-next-loc b f 5) => [5 (f (... (f b)...))]
@@ -27,44 +30,50 @@
           (recur (inc n) next-loc)
           [loc n])))))
 
+(defn- can-go-next? [vehicle passenger]
+  (and
+    (.isValid vehicle)
+    (.isValid passenger)
+    (= passenger (.getPassenger vehicle))))
+
 (defn- go-next [vehicle vehicle-loc passenger velocity]
-  (when (and
-          (.isValid vehicle)
-          (.isValid passenger)
-          (= passenger (.getPassenger vehicle)))
-    (let [direction (.normalize (.clone velocity))
-          next-loc-f (fn [vehicle-loc direction]
-                         (.getBlock
-                           (.add (.clone vehicle-loc) direction)))
-          [next-loc length] (find-next-loc vehicle-loc
-                                           (fn inc-f [loc]
-                                             (.add (.clone loc)
-                                                   direction))
-                                           5)]
-      (when (not= length 0)
-        (l/later 0
-          (w/play-sound next-loc Sound/ENTITY_MINECART_RIDING (float 0.2) (float 1.8))
-          (doseq [player (Bukkit/getOnlinePlayers)]
-            (l/later (l/sec 0.2)
-              (.sendBlockChange player next-loc Material/FIRE (byte 0)))
-            (l/later (l/sec 5)
-              (when (.isValid player)
-                (.sendBlockChange player next-loc (.getType (.getBlock next-loc)) (.getData (.getBlock next-loc))))))
-          (.setYaw next-loc (.getYaw (.getLocation passenger)))
-          (.setPitch next-loc (.getPitch (.getLocation passenger)))
-          (let [move (fn []
-                       (.teleport passenger (.add (.clone next-loc) 0 1 0))
-                       (.teleport vehicle next-loc)
-                       (.setPassenger vehicle passenger)
-                       (.setVelocity vehicle velocity))]
-            (.setFallDistance passenger 0)
-            (.setFallDistance vehicle 0)
-            (.addPotionEffect passenger
-                              (org.bukkit.potion.PotionEffect.
-                                org.bukkit.potion.PotionEffectType/FIRE_RESISTANCE
-                                20
-                                1))
-            (l/later length
+  (let [direction (.normalize (.clone velocity))
+        next-loc-f (fn [vehicle-loc direction]
+                     (.getBlock
+                       (.add (.clone vehicle-loc) direction)))
+        [next-loc length] (find-next-loc vehicle-loc
+                                         (fn inc-f [loc]
+                                           (.add (.clone loc)
+                                                 direction))
+                                         10)]
+    (when (not= length 0)
+      (.setFlyingVelocityMod vehicle (Vector.))
+      (w/play-sound next-loc Sound/ENTITY_MINECART_RIDING (float 0.2) (float 1.8))
+      (l/later 0
+        (doseq [player (Bukkit/getOnlinePlayers)]
+          (l/later (l/sec 0.5)
+            (.sendBlockChange player next-loc Material/FIRE (byte 0)))
+          (l/later (l/sec 5)
+            (when (.isValid player)
+              (.sendBlockChange player next-loc (.getType (.getBlock next-loc)) (.getData (.getBlock next-loc))))))
+        (let [move (fn []
+                     (.setYaw next-loc (.getYaw (.getLocation passenger)))
+                     (.setPitch next-loc (.getPitch (.getLocation passenger)))
+                     (let [next-loc-above (.add (.clone next-loc) 0 1 0)]
+                       (.teleport passenger next-loc-above)
+                       (.teleport vehicle next-loc-above))
+                     (.setPassenger vehicle passenger)
+                     (.setVelocity vehicle velocity))]
+          (.setFallDistance passenger 0)
+          (.setFallDistance vehicle 0)
+          (.addPotionEffect passenger
+                            (org.bukkit.potion.PotionEffect.
+                              org.bukkit.potion.PotionEffectType/FIRE_RESISTANCE
+                              20
+                              1))
+          (l/later length
+            (.setFlyingVelocityMod vehicle minecart-default-flying-velocity-mod)
+            (when (can-go-next? vehicle passenger)
               (move)
               (go-next vehicle next-loc passenger velocity))))))))
 
@@ -75,11 +84,13 @@
                  (instance? Player passenger))
         (let [player-name (.getName passenger)
               velocity (.getVelocity vehicle)]
-          (when (< minecart-max-speed (.length velocity))
+          (when (and (< minecart-max-speed (.length velocity))
+                     (can-go-next? vehicle passenger))
             (go-next vehicle
                      (.getLocation vehicle)
                      passenger
                      velocity)))))))
+
 
 #_(try
   #_(doseq [player (Bukkit/getOnlinePlayers)]
