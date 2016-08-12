f8390af4c617d703308abb9f485f611537cf0f04
Tatsuhiro Ujihisa <ujihisa@gmail.com>
2016-08-12 16:45:52 -0700

    WIP

--- src/spifax/app/bonus_boss.clj
+++ src/spifax/app/bonus_boss.clj
@@ -1,6 +1,15 @@
 (ns spifax.app.bonus-boss
   (:require [sugot.lib :as l]
-            [sugot.world :as w]))
+            [sugot.world :as w])
+  (:import [org.bukkit.entity Player EnderDragon]
+           [org.bukkit Material]
+           [org.bukkit.inventory ItemStack]))
 
 (defn org.bukkit.event.entity.EntityDeathEvent [event]
-  nil)
+  (let [living-entity (.getEntity event)
+        killer (.getKiller living-entity)]
+    (when (and
+            (instance? EnderDragon living-entity)
+            (instance? Player killer))
+      (let [killer-name (.getName killer)]
+        nil))))
