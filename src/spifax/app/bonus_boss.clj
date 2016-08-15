42cce2f40aa9a4fff3e613d19783a59d3ed3ef9c
ujihisa <ujihisa@gmail.com>
2016-08-14 17:14:03 -0700

    Let EnderDragon drops power6 special bow

--- src/spifax/app/bonus_boss.clj
+++ src/spifax/app/bonus_boss.clj
@@ -1,6 +1,7 @@
 (ns spifax.app.bonus-boss
   (:require [sugot.lib :as l]
-            [sugot.world :as w])
+            [sugot.world :as w]
+            [spifax.lib])
   (:import [org.bukkit.entity Player EnderDragon]
            [org.bukkit Material]
            [org.bukkit.inventory ItemStack]))
@@ -11,5 +12,10 @@
     (when (and
             (instance? EnderDragon living-entity)
             (instance? Player killer))
-      (let [killer-name (.getName killer)]
-        nil))))
+      (let [killer-name (.getName killer)
+            bow (spifax.lib/create-power6-named-bow killer-name)
+            msg (format "[BONUS_BOSS] %s killed an Ender Dragon. It dropped a special bow for %s!"
+                        killer-name killer-name)]
+        (w/drop-item (.getLocation killer) bow)
+        (l/broadcast msg)
+        (l/post-lingr msg)))))
