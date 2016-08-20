f75d82a245e9c4ef59e1c157ca8c412501211726
Tatsuhiro Ujihisa <ujihisa@gmail.com>
2016-08-19 18:56:17 -0700

    Spawn stray and wither skeletons randomely at hardcore world

--- src/spifax/app/hardcore.clj
+++ src/spifax/app/hardcore.clj
@@ -1,5 +1,10 @@
 (ns spifax.app.hardcore
-  (:require [sugot.app.hardcore]))
+  (:require [sugot.app.hardcore]
+            [sugot.world :as w]
+            [sugot.lib :as l])
+  (:import [org.bukkit.entity Skeleton]
+           [org.bukkit.event.entity CreatureSpawnEvent$SpawnReason]
+           [org.bukkit.entity Skeleton$SkeletonType]))
 
 (defn org.bukkit.event.player.PlayerDropItemEvent [event]
   (sugot.app.hardcore/PlayerDropItemEvent event))
@@ -23,7 +28,22 @@
   (sugot.app.hardcore/EntityDeathEvent event))
 
 (defn org.bukkit.event.entity.CreatureSpawnEvent [event]
-  (sugot.app.hardcore/CreatureSpawnEvent event))
+  (sugot.app.hardcore/CreatureSpawnEvent event)
+  (let [entity (.getEntity event)
+        reason (.getSpawnReason event)
+        l (.getLocation event)]
+    (when (and
+            (sugot.app.hardcore/loc-in-hardcore? l)
+            (instance? Skeleton entity)
+            (contains? #{CreatureSpawnEvent$SpawnReason/NATURAL
+                         CreatureSpawnEvent$SpawnReason/CHUNK_GEN
+                         CreatureSpawnEvent$SpawnReason/SPAWNER_EGG}
+                       reason)
+            (= 1 (rand-int 2)))
+      (l/later (l/sec 1)
+        (let [new-skeleton (w/spawn l Skeleton)
+              skeleton-type (rand-nth [Skeleton$SkeletonType/STRAY Skeleton$SkeletonType/WITHER])]
+          (.setSkeletonType new-skeleton skeleton-type))))))
 
 (defn org.bukkit.event.entity.ProjectileLaunchEvent [event]
   (sugot.app.hardcore/ProjectileLaunchEvent event))
