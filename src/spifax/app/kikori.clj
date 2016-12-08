99f87d9704199f2917284ef0bc131518d00c5b69
ryunix <ryunix.net@gmail.com>
2016-12-08 09:26:49 +0900

    Add kikori limit

--- src/spifax/app/kikori.clj
+++ src/spifax/app/kikori.clj
@@ -4,6 +4,8 @@
   (:import [org.bukkit Material Sound]
            [org.bukkit.event.block Action]))
 
+(def kikori-max 128)
+
 (defn- kikori-axe? [item]
   (and (= (.getType item) Material/WOOD_AXE)
        (= (l/get-display-name item) "Kikori's Axe")))
@@ -17,14 +19,15 @@
 (defn- wood-block? [block]
   (contains? #{Material/LOG Material/LOG_2} (.getType block)))
 
-(defn- kikori [base-block]
-  (when (wood-block? base-block)
+(defn- kikori [base-block kikori-count]
+  (when (and (< kikori-count kikori-max)
+             (wood-block? base-block))
     (.breakNaturally base-block)
     (doseq [y [0 1]
             x [-1 0 1]
             z [-1 0 1]]
       (let [block (.getRelative base-block x y z)]
-        (kikori block)))))
+        (kikori block (+ kikori-count 1))))))
 
 (defn org.bukkit.event.player.PlayerInteractEvent* [item action block player play-sound]
   (when (and (kikori-axe? item)
@@ -34,7 +37,7 @@
       (when (break-kikori-axe? item)
         (l/consume-item player)
         (play-sound Sound/ENTITY_ITEM_BREAK))
-      (kikori block))))
+      (kikori block 0))))
 
 (defn org.bukkit.event.player.PlayerInteractEvent [event]
   (#'org.bukkit.event.player.PlayerInteractEvent*
