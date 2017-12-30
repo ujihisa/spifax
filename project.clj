61ad839055129f7d48b2c26a1371f2d14e8c6fa7
ujihisa <ujihisa@gmail.com>
2017-12-30 12:51:27 +0900

    Update to minecraft 1.12

--- project.clj
+++ project.clj
@@ -1,24 +1,22 @@
-(defproject spifax "0.1.0-SNAPSHOT"
+(defproject spifax "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "GPL3 or any later versions"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :min-lein-version "2.5.0"
+  :min-lein-version "2.8.1"
   :repositories {"spigot-repo"
                  "https://hub.spigotmc.org/nexus/content/repositories/snapshots/"
-                 "raa0121"
-                 {:url "http://jenkins.raa0121.info/userContent/repository/" :checksum :warn}
+                 #_"raa0121"
+                 #_{:url "http://jenkins.raa0121.info/userContent/repository/" :checksum :warn}
                  "localrepo1"
                  "file://myrepo"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
-                 ;[org.spigotmc/spigot-api "1.10-R0.1-SNAPSHOT"]
-                 ;[org.spigotmc/spigot "1.10.2"]
-                 [clj-http "3.1.0"]
+                 [clj-http "3.7.0"]
                  ; I don't know why, but you can't let `sugot` fetch `spigot` and `spigot-api`,
                  ; otherwise you'll get a runtime error at ring bootup
-                 [sugot "1.11-SNAPSHOT" :exclusions [com.google.guava/guava
+                 [sugot "1.12-SNAPSHOT" :exclusions [com.google.guava/guava
                                                      com.google.code.gson/gson
                                                      clj-http]]]
   :plugins [[lein-ring "0.9.7"]]
